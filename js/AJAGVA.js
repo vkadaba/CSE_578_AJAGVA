be3286fc5be69341bae824f21922f5538d61642f
Alexander Gaouette <72415919+agaouett@users.noreply.github.com>
2023-11-21 20:30:28 -0700

    Added range select and converted all times to UTC

--- js/AJAGVA.js
+++ js/AJAGVA.js
@@ -3,8 +3,10 @@ const width = 1000;
 const height = 800;
 const innerWidth = width - margin.left - margin.right;
 const innerHeight = height - margin.top - margin.bottom;
+const utcFormat = d3.utcFormat("%m/%d/%Y %H:%M:%S")
 var gps_data, car_assignments, cc_data, loyalty_data, abila;
-var tooltip, projection, map_path, selected_vehicle;
+var tooltip, projection, map_path, selected_vehicle, selected_range;
+var range_start, range_end;
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -17,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
     ];
 
     Promise.all(import_files).then(function (values) {
-        
+        populateRangeSelects();
         gps_data = values[0];
         car_assignments = values[1];
         cc_data = values[2];
@@ -44,6 +46,8 @@ document.addEventListener('DOMContentLoaded', function () {
         projection = d3.geoMercator().fitSize([innerWidth, innerHeight], abila);
         map_path = d3.geoPath().projection(projection);
 
+       
+        
         drawMap();
         selectVehicle();
     });
@@ -103,9 +107,9 @@ function selectVehicle() {
 };
 
 function dataWrangle() {
-    var timeParse = d3.timeParse('%m/%d/%Y %H:%M:%S')
+    var utcParse = d3.utcParse('%m/%d/%Y %H:%M:%S')
     temp = gps_data.map(d => ({
-        "timestamp": timeParse(d.Timestamp),
+        "timestamp": utcParse(d.Timestamp).getTime(),
         "CarID": +d.id,
         // Coordinate structure [lat, long]
         "coords": [+d.lat, +d.long]
@@ -123,23 +127,128 @@ function dataWrangle() {
 
     car_assignments = temp;
 
-    timeParse = d3.timeParse('%m/%d/%Y %H:%M')
+    utcParse = d3.utcParse('%m/%d/%Y %H:%M')
     temp = cc_data.map(d => ({
         "last4": +d.last4ccnum,
         "location": d.location,
         "price": +d.price,
-        "timestamp": timeParse(d.timestamp)
+        "timestamp": utcParse(d.timestamp).getTime()
     }));
 
     cc_data = temp;
 
-    timeParse = d3.timeParse('%m/%d/%Y')
+    utcParse = d3.utcParse('%m/%d/%Y')
     temp = loyalty_data.map(d => ({
         "localtion": d.location,
         "loyaltynum": d.loyaltynum,
         "price": +d.price,
-        "timestamp": timeParse(d.timestamp)
+        "timestamp": utcParse(d.timestamp).getTime()
     }));
 
     loyalty_data = temp;
+}
+
+// Ranges are not validated!
+// All time is UTC / Unix Epoch Time and must be necessarily converted!
+function updateRange() {
+    console.log("Time Range Updated!")
+    var start_day = +d3.select('#range-start-day').node().value;
+    var start_hour = +d3.select("#range-start-hours").node().value;
+    range_start = start_day + start_hour;
+    console.log(`Range Start: ${utcFormat(range_start)}`);
+
+    var end_day = +d3.select('#range-end-day').node().value;
+    var end_hour = +d3.select("#range-end-hours").node().value;
+    range_end = end_day + end_hour;
+    console.log(`Range End: ${utcFormat(range_end)}`);
+}
+
+function populateRangeSelects() {
+    days = [
+        Date.UTC(2014, 0, 6, 0, 0, 0),
+        Date.UTC(2014, 0, 7, 0, 0, 0),
+        Date.UTC(2014, 0, 8, 0, 0, 0),
+        Date.UTC(2014, 0, 9, 0, 0, 0),
+        Date.UTC(2014, 0, 10, 0, 0, 0),
+        Date.UTC(2014, 0, 11, 0, 0, 0),
+        Date.UTC(2014, 0, 12, 0, 0, 0),
+        Date.UTC(2014, 0, 13, 0, 0, 0),
+        Date.UTC(2014, 0, 14, 0, 0, 0),
+        Date.UTC(2014, 0, 15, 0, 0, 0),
+        Date.UTC(2014, 0, 16, 0, 0, 0),
+        Date.UTC(2014, 0, 17, 0, 0, 0),
+        Date.UTC(2014, 0, 18, 0, 0, 0),
+        Date.UTC(2014, 0, 19, 0, 0, 0),
+        Date.UTC(2014, 0, 20, 0, 0, 0)
+    ];
+
+    hours = [
+        [0, '00:00'],
+        [21600000, '06:00'],
+        [43200000, '12:00'],
+        [64800000, '18:00']
+    ];
+
+    var rsd = d3.select('#range-start-day');
+
+    rsd.selectAll('option')
+        .data(days)
+        .enter()
+        .append('option')
+        .attr('value', function(d,i) {
+            return days[i];
+        })
+        .text(function(d, i) {
+            return d3.utcFormat("%m/%d/%Y")(days[i]);
+        });
+
+    var rsh = d3.select('#range-start-hours');
+
+    rsh.selectAll('option')
+        .data(hours)
+        .enter()
+        .append('option')
+        .attr('value', function(d,i) {
+            return hours[i][0];
+        })
+        .text(function(d, i) { 
+            return hours[i][1];
+        });
+    
+    var red = d3.select('#range-end-day');
+
+    red.selectAll('option')
+        .data(days)
+        .enter()
+        .append('option')
+        .attr('value', function(d,i) {
+            return days[i]
+        })
+        .text(function(d, i) { 
+            return d3.utcFormat("%m/%d/%Y")(days[i]);
+        });
+
+    var reh = d3.select('#range-end-hours');
+
+    reh.selectAll('option')
+        .data(hours)
+        .enter()
+        .append('option')
+        .attr('value', function(d,i) {
+            return hours[i][0]
+        })
+        .text(function(d, i) { 
+            return hours[i][1];
+        });
+        
+    console.log("Time Range Updated!")
+    var start_day = +d3.select('#range-start-day').node().value;
+    var start_hour = +d3.select("#range-start-hours").node().value;
+    range_start = start_day + start_hour;
+    console.log(`Range Start: ${utcFormat(range_start)}`);
+
+    var end_day = +d3.select('#range-end-day').node().value;
+    var end_hour = +d3.select("#range-end-hours").node().value;
+    range_end = end_day + end_hour;
+    console.log(`Range End: ${utcFormat(range_end)}`);
 }
